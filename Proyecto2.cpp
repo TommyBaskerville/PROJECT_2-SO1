ce5efbdbd243b9114c78ed9e14df97eb4f561d48
TommyBskerville <luillixd@outlook.es>
2023-10-20 07:34:35 -0600

    Hilo generador de elementos

--- Proyecto2.cpp
+++ Proyecto2.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<thread>
+#include<chrono>
 #include<stdlib.h>
 using namespace std;
 
@@ -12,6 +14,7 @@ void menu();
 void insertarCola(Nodo *&,Nodo *&,char,int);
 bool cola_vacia(Nodo *);
 void suprimirCola(Nodo *&,Nodo *&,char &,int &);
+void generarElementos(Nodo *&, Nodo *&);
 
 int main(){
     int contador = rand() % 81 + 20; // Generar un número aleatorio entre 20 y 100
@@ -27,15 +30,7 @@ void menu(){
     Nodo *frente = NULL;
     Nodo *fin = NULL;
 
-    int n = 15; // number of elements to add
-    int i = 0; // counter for loop
-
-    while (i < n) {
-        char dato = 'A' + i; // generate a character to add to the queue
-        int prioridad = rand() % 3 + 1; // generate a random priority
-        insertarCola(frente, fin, dato, prioridad); // insert the element into the queue
-        i++; // increment the counter
-    }
+    thread generador(generarElementos, ref(frente), ref(fin)); // Crear un hilo para generar elementos
 
     do{
         system("clear"); // Limpiar la pantalla en Linux
@@ -73,6 +68,8 @@ void menu(){
             break;
         }
     } while (opc != 3);
+
+    generador.join(); // Esperar a que el hilo termine
 }
 
 void insertarCola(Nodo *&frente, Nodo *&fin, char dato, int prioridad){
@@ -121,4 +118,15 @@ void suprimirCola(Nodo *&frente, Nodo *&fin, char &dato, int &prioridad){
 
 bool cola_vacia(Nodo *frente){
     return (frente == NULL);
+}
+
+void generarElementos(Nodo *&frente, Nodo *&fin){
+    int i = 0;
+    while (true) {
+        char dato = 'A' + i; // generar un nuevo elemento
+        int prioridad = rand() % 3 + 1; // generar una prioridad aleatoria
+        insertarCola(frente, fin, dato, prioridad); // insertar el elemento en la cola
+        i++; // incrementar el contador
+        this_thread::sleep_for(chrono::seconds(1)); // esperar un segundo antes de generar el siguiente elemento
+    }
 }
